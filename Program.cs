9d7058eb3d088f141eb3e0c4e2da455263248373
opilane <opilane@HP-CZC2349HT3>
2023-09-01 10:53:53 +0300

    123

--- Program.cs
+++ Program.cs
@@ -5,23 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
 namespace OOP
 {
     internal class Program
     {
 
-        public class StudentMain
+        //public class StudentMain
+        //{
+        //    public static void Main()
+        //    {
+        //        Student student = new Student("John", 17, "London");
+
+
+        //        //Console.WriteLine(student.Name);
+        //        //Console.WriteLine(student.Age);
+        //        //Console.WriteLine(student.GetCity());
+        //        student.ShowInfo();
+        //    }
+        //}
+
+
+
+
+
+        public static void Main()
         {
-            public static void Main()
-            {
-                Student student = new Student("John", 17, "London");
+            Message m1 = new Message("Hello", "John", DateTime.Now.AddDays(-10));
+            m1.Messages();
+            Console.WriteLine(m1.Time);
+            m1.AddLike();
+            Console.WriteLine(m1.GetPopularity());
+
+            Message m2 = new Message("Hi", "Mary", DateTime.Now.AddMinutes(-1));
+            m1.Messages();
+            for (int i = 0;i < 100; i++) { m2.AddLike(); }
+            Console.WriteLine(m2.GetPopularity());
+            Console.WriteLine(m1.ShowPopularity(m1.GetPopularity(),m2.GetPopularity()));
 
 
-                //Console.WriteLine(student.Name);
-                //Console.WriteLine(student.Age);
-                //Console.WriteLine(student.GetCity());
-                student.ShowInfo();
-            }
+            Message m3 = new Message("Tere", "Mari", DateTime.Now.AddDays(-1));
+            Message m4 = new Message("Guten Tag", "Hans", DateTime.Now.AddDays(-5));
+            Message m5 = new Message("Hei", "Marika", DateTime.Now.AddDays(-10));
+            List<Message> list = new List<Message>();
+            list.Add(m1);
+            list.Add(m2);
+            list.Add(m3);
+            list.Add(m4);
+            list.Add(m5);
+            Console.WriteLine(m1.ShowPopularityN(list));
+
+
         }
     }
 }
--- Students.cs
+++ Students.cs
@@ -5,62 +5,143 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace OOP
 {
-    public class Student
+    //public class Student
+    //{
+    //    public string Name { get; }
+    //    public int age;
+    //    private readonly string _city;
+    //    public string status;
+
+    //    public Student(string name, int age, string city)
+    //    {
+    //        Name = name;
+    //        Age = age;
+    //        _city = city;
+    //    }
+    //    public int Age
+    //    {
+    //        get { return age; }
+    //        set
+    //        {
+    //            age = value;
+    //            if (age < 18) status = "alapealine";
+    //            else status = "täiskasvanud";
+    //        }
+
+    //    }
+
+
+    //    public int Birth()
+    //    {
+    //        int currentData= DateTime.Now.Year;
+    //        return currentData;
+    //    }
+
+    //    public string Status
+    //    {
+    //        get { return status; }
+    //    }
+
+
+    //    public string GetCity()
+    //    {
+    //        return _city;
+    //    }
+
+    //    public void ShowInfo()
+    //    {
+
+    //        Console.WriteLine(Name);
+    //        Console.WriteLine(Age);
+    //        Console.WriteLine(GetCity());
+    //        Console.WriteLine(Status);
+    //        Console.WriteLine(Birth());
+
+
+    //    }
+    //}
+
+
+    public class Message
     {
-        public string Name { get; }
-        public int age;
-        private readonly string _city;
-        public string status;
+        private readonly string _content;
+        private readonly string _author;
+        private readonly DateTime _time;
+        private int _likes;
 
-        public Student(string name, int age, string city)
+        public Message(string content, string author, DateTime time)
         {
-            Name = name;
-            Age = age;
-            _city = city;
+            this._content = content;
+            this._author = author;
+            this._time = time;
         }
-        public int Age
+
+        public int Likes { get => _likes; }
+        public DateTime Time { get => _time; }
+        public string Author { get => _author; }
+        public string Content { get => _content; }
+
+        public void AddLike()
         {
-            get { return age; }
-            set
+            _likes++;
+        }
+
+        public double GetPopularity()
+        {
+            double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
+            if (elapsed == 0)
             {
-                age = value;
-                if (age < 18) status = "alapealine";
-                else status = "täiskasvanud";
+                return _likes;
             }
-        
+            return _likes / elapsed;
+
         }
 
-
-        public int Birth()
+        public void Messages()
         {
-            int currentData= DateTime.Now.Year;
-            return currentData;
-        }
-
-        public string Status
-        {
-            get { return status; }
+            Console.WriteLine("Sisu:{0}\nAutor: {1}", Content, Author);
         }
 
 
-        public string GetCity()
-        {
-            return _city;
-        }
-
-        public void ShowInfo()
+        public string ShowPopularity(double esimene, double teine)
         {
 
-            Console.WriteLine(Name);
-            Console.WriteLine(Age);
-            Console.WriteLine(GetCity());
-            Console.WriteLine(Status);
-            Console.WriteLine(Birth());
-
+            string  result = "";
+            if (esimene > teine) { result = "Esimene sõune on polaarsem kui teine"; };
+            if (esimene < teine) { result = "Teine sõune on polaarsem kui Esimene"; };
+            return result;
 
         }
+
+        public string ShowPopularityN(List<Message> messages)
+        {
+
+            string result = "";
+            double popularity = 0;
+            for (int i = 0; 1 < messages.Count; i++)
+            {
+                if (messages[i].GetPopularity()>popularity) 
+                {
+                    popularity = messages[i].GetPopularity();
+                    result = messages[i].Content + " on kõige populaarse sõnum, seda kirjutas " + messages[i].Author;
+                    
+                }
+            }
+            return result;
+                                            
+            
+
+
+  
+            
+
+        }
+
+
+
     }
 }
 
